d832fed3b7ac04d95ceee3e4feebb22885f9a006
omkar07b <124290488+omkar07b@users.noreply.github.com>
2025-07-04 17:17:38 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+result = factorial(a)
+
+print("Factorial of", a, "is:", result)
---
+++ Task2.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a number: "))
+
+import math
+
+print("Square root:", math.sqrt(a))
+print("Logarithmic:", math.log(a))
+print("Sine:", math.sin(a))
